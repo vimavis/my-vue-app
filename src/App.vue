de0361b8a4501ddf642938f9c9e3cfa1d83a4481
Victor Vasquez <vvasquezs@laaraucana.cl>
2024-01-23 16:03:47 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<script setup>
+import Song from "./components/Song.vue";
+import { onMounted, ref } from 'vue'
+
+const listSongs = ref([]);
+const status = ref('loading');
+
+onMounted(async () => {
+  const url = 'https://musicbrainz.org/ws/2/release/b84ee12a-09ef-421b-82de-0441a926375b?inc=recordings&fmt=json';
+  try {
+    const response = await (await fetch(url)).json();
+    listSongs.value = response.media[0].tracks.map((song) => {
+      return {
+        index: song.position,
+        name: song.title,
+        timeMs: song.length,
+        active: false,
+      };
+    });
+    status.value = 'ok';
+  } catch (error) {
+    status.value = 'error';
+  }
+  console.log(listSongs);
+})
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="border">album</div>
+    <div class="border">
+      <div class="headers">
+        <span>#</span>
+        <span>Nombre</span>
+        <span>Duracion</span>
+      </div>
+      <song
+        v-for="song in listSongs"
+        :index="song.index"
+        :name="song.name"
+        :time-ms="song.timeMs"
+        :active="false"
+      ></song>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  height: 100vh;
+  width: 100vw;
+  display: grid;
+  grid-template-rows: 100px 1fr;
+}
+
+.border {
+  border: solid 2px red;
+}
+
+.headers {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
+
+<!-- 
+index: Number,
+name: String,
+timeMs: Number,
+active: Boolean -->
---
+++ src/components/Song.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+  index: Number,
+  name: String,
+  timeMs: Number,
+  active: Boolean
+})
+
+const timeMMSS = computed(() => {
+  const minutes = Math.floor(props.timeMs/1000/60);
+  const seconds = Math.floor((props.timeMs/1000)) % 60;
+  return `${minutes}:${seconds}`;
+})
+
+</script>
+
+<template>
+  <div class="song">
+    <button @click="" >play</button>
+    <span>{{ index }}</span>
+    <span>{{ name }}</span>
+    <span>{{ timeMMSS }}</span>
+  </div>
+</template>
+
+<style scoped>
+.song {
+  display: flex;
+  justify-content: space-around;
+}
+</style>
